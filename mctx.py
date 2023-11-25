dfb865b663795646853f5892fc240cc7043674f7
Akulen <tomas@rigaux.com>
2023-11-25 22:03:24 +0900

    Added aim

--- mctx.py
+++ mctx.py
@@ -1,9 +1,12 @@
+from aim import Run
 from functools import partial
+import humanhash
 import jax
 import jax.numpy as jnp
 # import mctx
 import optax
 import pgx
+import rich.progress as rp
 
 from jpyger import state_to_graph
 from models import EdgeNet
@@ -12,7 +15,7 @@ devices = jax.local_devices()
 num_devices = len(devices)
 
 config = {
-    'n_iter': 2,
+    'n_iter': 10,
     'eval_interval': 1,
     'selfplay_batch_size': 12, # 1024,
 }
@@ -67,6 +70,12 @@ def evaluate(rng_key, params):
 
 
 if __name__ == "__main__":
+    run = Run(
+        repo='aim://localhost:53800',
+        experiment='mctx_dev',
+    )
+    run["hparams"] = config
+
     model = EdgeNet()
     optimizer = optax.adam(learning_rate=0.001)
 
@@ -87,55 +96,54 @@ if __name__ == "__main__":
     # print(state_to_graph(state))
 
     rng_key = jax.random.PRNGKey(42)
-    for iteration in range(config['n_iter']):
-        if iteration % config['eval_interval'] == 0:
-            # Evaluation
-            rng_key, subkey = jax.random.split(rng_key)
-            keys = jax.random.split(subkey, num_devices)
-            R = evaluate(keys, params)
-            print(R)
-            # pgx.save_svg_animation(states[:n_iter+1], f"gardner_test.svg", frame_duration_seconds=0.5)
-            import sys
-            sys.exit()
-            raise NotImplementedError
-            # log.update(
-            #     {
-            #         f"eval/vs_baseline/avg_R": R.mean().item(),
-            #         f"eval/vs_baseline/win_rate": ((R == 1).sum() / R.size).item(),
-            #         f"eval/vs_baseline/draw_rate": ((R == 0).sum() / R.size).item(),
-            #         f"eval/vs_baseline/lose_rate": ((R == -1).sum() / R.size).item(),
-            #     }
-            # )
+    with rp.Progress(
+        *rp.Progress.get_default_columns(),
+        rp.TimeElapsedColumn(),
+        rp.MofNCompleteColumn(),
+        rp.TextColumn("win/lose rate: {task.fields[win]} / {task.fields[lose]}"),
+    ) as progress:
+        task = progress.add_task("[cyan]Iteration", total=config['n_iter'], win='N/A', lose='N/A')
+        for iteration in range(config['n_iter']):
+            if iteration % config['eval_interval'] == 0:
+                # Evaluation
+                rng_key, subkey = jax.random.split(rng_key)
+                keys = jax.random.split(subkey, num_devices)
+                R = evaluate(keys, params)
+                print(R)
+                avg_R = R.mean().item()
+                win_rate, draw_rate, lose_rate = map(
+                    lambda r: ((R == r).sum() / R.size).item(),
+                    [1, 0, -1]
+                )
+                run.track({
+                    "eval/vs_baseline/avg_R": avg_R,
+                    "eval/vs_baseline/win_rate": win_rate,
+                    "eval/vs_baseline/draw_rate": draw_rate,
+                    "eval/vs_baseline/lose_rate": lose_rate,
+                }, epoch=iteration)
+                progress.update(
+                    task,
+                    win=f"{win_rate:.3f}",
+                    lose=f"{lose_rate:.3f}",
+                )
 
-            # # Store checkpoints
-            # model_0, opt_state_0 = jax.tree_util.tree_map(lambda x: x[0], (model, opt_state))
-            # with open(os.path.join(ckpt_dir, f"{iteration:06d}.ckpt"), "wb") as f:
-            #     dic = {
-            #         "config": config,
-            #         "rng_key": rng_key,
-            #         "model": jax.device_get(model_0),
-            #         "opt_state": jax.device_get(opt_state_0),
-            #         "iteration": iteration,
-            #         "frames": frames,
-            #         "hours": hours,
-            #         "pgx.__version__": pgx.__version__,
-            #         "env_id": env.id,
-            #         "env_version": env.version,
-            #     }
-            #     pickle.dump(dic, f)
-        raise NotImplementedError
-    # states = []
-    # batch_size = 2
-    # keys = jax.random.split(jax.random.PRNGKey(0), batch_size)
-    # state = init_fn(keys)
-    # print(state_to_graph(state))
+                # # Store checkpoints
+                # model_0, opt_state_0 = jax.tree_util.tree_map(lambda x: x[0], (model, opt_state))
+                # with open(os.path.join(ckpt_dir, f"{iteration:06d}.ckpt"), "wb") as f:
+                #     dic = {
+                #         "config": config,
+                #         "rng_key": rng_key,
+                #         "model": jax.device_get(model_0),
+                #         "opt_state": jax.device_get(opt_state_0),
+                #         "iteration": iteration,
+                #         "frames": frames,
+                #         "hours": hours,
+                #         "pgx.__version__": pgx.__version__,
+                #         "env_id": env.id,
+                #         "env_version": env.version,
+                #     }
+                #     pickle.dump(dic, f)
+            progress.update(task, advance=1)
 
-    # states.append(state)
-    # while not (state.terminated | state.truncated).all():
-    #     logits, value = baseline(state.observation)
-    #     action = logits.argmax(-1)
-    #     state = step_fn(state, action)
-    #     states.append(state)
-
-    # pgx.save_svg_animation(states, f"{env_id}.svg", frame_duration_seconds=0.5)
+        # pgx.save_svg_animation(states, f"{env_id}.svg", frame_duration_seconds=0.5)
 
